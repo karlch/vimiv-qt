7d3cc142c001da15f4f9e43978a06552a0447335
karlch <karlch@protonmail.com>
2017-12-17 15:49:15 +0100

    Remove tests for removed function init_paths

--- tests/unit/test_startup.py
+++ tests/unit/test_startup.py
@@ -17,42 +17,6 @@ def test_parser_description():
     assert "image viewer" in parser.description
 
 
-def test_init_paths_with_images(mocker):
-    mocker.patch("vimiv.utils.files.get_supported",
-                 return_value=(["dir/image.jpg"], []))
-    mocker.patch("os.chdir")
-    assert startup.init_paths(["any"]) == ["dir/image.jpg"]
-    os.chdir.assert_called_once_with(os.path.abspath("dir"))
-
-
-def test_init_paths_with_directory(mocker):
-    mocker.patch("vimiv.utils.files.get_supported",
-                 return_value=([], ["dir"]))
-    mocker.patch("os.chdir")
-    assert startup.init_paths(["any"]) is None
-    os.chdir.assert_called_once_with("dir")
-
-
-def test_init_paths_with_both(mocker):
-    mocker.patch("vimiv.utils.files.get_supported",
-                 return_value=(["foo/bar.png"], ["dir"]))
-    mocker.patch("os.chdir")
-    mocker.patch("logging.warning")
-    assert startup.init_paths(["any"]) == ["foo/bar.png"]
-    os.chdir.assert_called_once_with(os.path.abspath("foo"))
-    logging.warning.assert_called_once()
-
-
-def test_init_paths_with_multiple_directories(mocker):
-    mocker.patch("vimiv.utils.files.get_supported",
-                 return_value=([], ["dir1", "dir2"]))
-    mocker.patch("os.chdir")
-    mocker.patch("logging.warning")
-    assert startup.init_paths(["any"]) is None
-    os.chdir.assert_called_once_with("dir1")
-    logging.warning.assert_called_once()
-
-
 def test_init_directories(mocker):
     mocker.patch("os.path.isdir", return_value=False)
     mocker.patch("os.mkdir")
