c25011edd3a8382ed716f9242236803c68eec118
karlch <karlch@protonmail.com>
2017-12-19 17:41:53 +0100

    Add a few tests for styles module

---
+++ tests/integration/test_style.py
@@ -0,0 +1,26 @@
+# vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+"""Tests for vimiv.config.styles which require files."""
+
+import os
+
+import pytest
+
+from vimiv.config import styles
+
+
+def test_dump_and_read_default_style(mocker, tmpdir):
+    # Create default
+    styles.create_default()
+    # Write to file
+    confdir = str(tmpdir)
+    tmpdir.mkdir("styles")
+    mocker.patch("vimiv.utils.xdg.get_vimiv_config_dir", return_value=confdir)
+    styles.dump("default")
+    assert os.path.isfile(tmpdir.join("styles", "default"))
+    # Clear to make sure the values did not just stay
+    styles.clear()
+    with pytest.raises(KeyError):
+        styles.get("image.bg")
+    styles.read("default")
+    # Very simple assertion as default values can change
+    assert "#" in styles.get("image.bg")
---
+++ tests/unit/config/test_styles.py
@@ -0,0 +1,35 @@
+# vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+
+import pytest
+
+from vimiv.config import styles
+
+
+@pytest.fixture
+def new_style():
+    new_style = styles.Style("new_style")
+    yield new_style
+    del new_style
+
+
+def test_add_style_option(new_style):
+    new_style["red"] = "#ff0000"
+    assert new_style["{red}"] == "#ff0000"
+
+
+def test_fail_add_non_string_style_option(new_style):
+    with pytest.raises(AssertionError, match="must be string"):
+        new_style[True] = "#ff0000"
+
+
+def test_fail_add_non_string_style_value(new_style):
+    with pytest.raises(AssertionError, match="must be string"):
+        new_style["red"] = 12
+
+
+def test_replace_referenced_variables(mocker, new_style):
+    mocker.patch.object(styles, "get_current", return_value=new_style)
+    new_style["red"] = "#ff0000"
+    new_style["error.fg"] = "{red}"
+    styles.replace_referenced_variables()
+    assert new_style["{error.fg}"] == "#ff0000"
