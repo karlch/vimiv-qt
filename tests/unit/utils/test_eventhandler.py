212474b45216bc80b39c2f1bf9ad8de825432980
karlch <karlch@protonmail.com>
2017-12-25 01:39:54 +0100

    Update keynames in eventhandler test

--- tests/unit/utils/test_eventhandler.py
+++ tests/unit/utils/test_eventhandler.py
@@ -38,22 +38,22 @@ def test_keyevent_to_string_for_uppercase_letter():
 
 def test_keyevent_to_string_for_special_key():
     event = QKeyEvent(QEvent.KeyPress, Qt.Key_Tab, Qt.NoModifier, "\t")
-    assert eventhandler.keyevent_to_string(event) == "tab"
+    assert eventhandler.keyevent_to_string(event) == "<tab>"
 
 
 def test_keyevent_to_string_for_special_key_with_shift():
     event = QKeyEvent(QEvent.KeyPress, Qt.Key_Tab, Qt.ShiftModifier, "\t")
-    assert eventhandler.keyevent_to_string(event) == "shift+tab"
+    assert eventhandler.keyevent_to_string(event) == "<shift><tab>"
 
 
 def test_keyevent_to_string_for_control_modifier_and_letter():
     event = QKeyEvent(QEvent.KeyPress, Qt.Key_A, Qt.ControlModifier, "a")
-    assert eventhandler.keyevent_to_string(event) == "ctrl+a"
+    assert eventhandler.keyevent_to_string(event) == "<ctrl>a"
 
 
 def test_keyevent_to_string_for_alt_modifier_and_letter():
     event = QKeyEvent(QEvent.KeyPress, Qt.Key_A, Qt.AltModifier, "a")
-    assert eventhandler.keyevent_to_string(event) == "alt+a"
+    assert eventhandler.keyevent_to_string(event) == "<alt>a"
 
 
 def test_keyevent_to_string_for_only_modifier():
@@ -63,4 +63,4 @@ def test_keyevent_to_string_for_only_modifier():
 
 def test_keyevent_to_string_for_colon():
     event = QKeyEvent(QEvent.KeyPress, Qt.Key_Colon, Qt.NoModifier, ":")
-    assert eventhandler.keyevent_to_string(event) == "colon"
+    assert eventhandler.keyevent_to_string(event) == "<colon>"
