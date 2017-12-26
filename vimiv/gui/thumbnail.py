9db07e360d3884abe35f7412ca2b50c42f4e260b
karlch <karlch@protonmail.com>
2017-12-26 16:36:34 +0100

    Add commands to scroll in thumbnail mode

--- vimiv/gui/thumbnail.py
+++ vimiv/gui/thumbnail.py
@@ -3,11 +3,12 @@
 
 import os
 
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import Qt, QSize, QItemSelectionModel
 from PyQt5.QtWidgets import QListWidget, QListWidgetItem
 from PyQt5.QtGui import QIcon
 
-from vimiv.config import styles
+from vimiv.commands import commands, argtypes
+from vimiv.config import styles, keybindings
 from vimiv.modes import modehandler
 from vimiv.gui import statusbar
 from vimiv.utils import impaths, objreg, eventhandler
@@ -89,12 +90,80 @@ class ThumbnailView(eventhandler.KeyHandler, QListWidget):
     def _on_enter(self, widget):
         if widget == "thumbnail":
             self._stack.setCurrentWidget(self)
+            self._select_item(0)
 
     def _on_leave(self, widget):
         # Need this here in addition to _on_enter in image because we may leave
         # for the library
         if widget == "thumbnail":
             self._stack.setCurrentWidget(objreg.get("image"))
+
+    @keybindings.add("k", "scroll up", mode="thumbnail")
+    @keybindings.add("j", "scroll down", mode="thumbnail")
+    @keybindings.add("h", "scroll left", mode="thumbnail")
+    @keybindings.add("l", "scroll right", mode="thumbnail")
+    @commands.argument("direction", type=argtypes.scroll_direction)
+    @commands.register(instance="thumbnail", mode="thumbnail", count=1)
+    def scroll(self, direction, count):
+        """Scroll thumbnails.
+
+        Args:
+            direction: One of "right", "left", "up", "down".
+        """
+        # print(self.columns())
+        current = self.currentRow()
+        if direction == "right":
+            current += 1
+        elif direction == "left":
+            current -= 1
+        elif direction == "down":
+            current += self.columns()
+        else:
+            current -= self.columns()
+        current = max(0, current)
+        current = min(current, self.count() - 1)
+        self._select_item(current)
+
+    @keybindings.add("gg", "goto 1", mode="thumbnail")
+    @keybindings.add("G", "goto -1", mode="thumbnail")
+    @commands.argument("item", type=int)
+    @commands.register(instance="thumbnail", mode="thumbnail", count=0)
+    def goto(self, item, count):
+        """Select thumbnail.
+
+        Args:
+            item: Number of the item to select if no count is given.
+                -1 is the last item.
+        """
+        item = count if count else item  # Prefer count
+        if item > 0:
+            item -= 1  # Start indexing at 1
+        item = item % self.count()
+        self._select_item(item)
+
+    def _select_item(self, index):
+        """Select specific item in the ListWidget.
+
+        Args:
+            index: Number of the current item to select.
+        """
+        index = self.model().index(index, 0)
+        self._select_index(index)
+
+    def _select_index(self, index):
+        """Select specific index in the ListWidget.
+
+        Args:
+            index: QModelIndex to select.
+        """
+        selmod = QItemSelectionModel.Rows | QItemSelectionModel.ClearAndSelect
+        self.selectionModel().setCurrentIndex(index, selmod)
+
+    def columns(self):
+        """Return the number of columns."""
+        padding = 2 * int(styles.get("thumbnail.padding"))
+        item_width = self.iconSize().width() + padding
+        return (self.width() - padding - 2) // item_width
 
     @statusbar.module("{thumbnail_name}", instance="thumbnail")
     def current(self):
