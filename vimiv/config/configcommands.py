eb1d4a694a562ac88db2f0efc8e74e2f20158464
karlch <karlch@protonmail.com>
2017-12-27 21:24:31 +0100

    Add more settings-changing keybindings

--- vimiv/config/configcommands.py
+++ vimiv/config/configcommands.py
@@ -10,6 +10,10 @@ from vimiv.config import settings, keybindings
 from vimiv.utils import strconvert
 
 
+@keybindings.add(".", "set slideshow.delay +0.5", mode="image")
+@keybindings.add(",", "set slideshow.delay -0.5", mode="image")
+@keybindings.add("H", "set library.width -20", mode="library")
+@keybindings.add("L", "set library.width +20", mode="library")
 @keybindings.add("b", "set statusbar.show!")
 @commands.argument("value", nargs="*")
 @commands.argument("setting")
