13155c4a3e0d9666ff48d47e27a3f42b2bd13e51
karlch <karlch@protonmail.com>
2018-01-05 02:56:49 +0100

    Delete init_directories test
    As the temporary basedir will be used for the end2end tests this will be
    tested their implicitly.

--- tests/unit/test_startup.py
+++ tests/unit/test_startup.py
@@ -19,17 +19,3 @@ def test_parser_prog_name():
 def test_parser_description():
     parser = startup.get_argparser()
     assert "image viewer" in parser.description
-
-
-def test_init_directories(mocker):
-    mocker.patch("os.path.isdir", return_value=False)
-    mocker.patch("os.mkdir")
-    mocker.patch("vimiv.utils.xdg.get_vimiv_cache_dir", return_value="cache")
-    mocker.patch("vimiv.utils.xdg.get_vimiv_config_dir", return_value="config")
-    mocker.patch("vimiv.utils.xdg.get_vimiv_data_dir", return_value="data")
-    startup.init_directories()
-    # Check if all directories were created
-    os.mkdir.assert_any_call("cache")
-    os.mkdir.assert_any_call("config")
-    os.mkdir.assert_any_call("data")
-    os.mkdir.assert_any_call("config/styles")
