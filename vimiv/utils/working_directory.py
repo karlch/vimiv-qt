00c432c108fb486ba0db63e4176b5c287472cae4
karlch <karlch@protonmail.com>
2019-01-01 15:55:01 +0100

    Add watchdog observer for working directory
    This will allow re-loading library, image, ... when things in the
    directory changed.

--- vimiv/utils/working_directory.py
+++ vimiv/utils/working_directory.py
@@ -6,9 +6,12 @@
 
 """Handler to take care of the current working directory."""
 
+import logging
 import os
 
 from PyQt5.QtCore import QObject, pyqtSignal
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
 class WorkingDirectoryHandler(QObject):
@@ -17,13 +20,33 @@ class WorkingDirectoryHandler(QObject):
     Signals:
         cwd_changed: Emitted when the current working directory has changed.
             arg1: The new working directory.
+
+    Attributes:
+        _dir: The current working directory.
+        _event_handler: EventHandler object used by _observer to react to
+            changes to the directory content.
+        _observer: Monitoring observer that emits signals when the directory
+            content has changed.
     """
 
     cwd_changed = pyqtSignal(str)
 
+    # TODO check which of these will be needed
+    path_created = pyqtSignal(str)
+    path_deleted = pyqtSignal(str)
+    path_modified = pyqtSignal(str)
+    path_moved = pyqtSignal(str, str)
+
+    dir_created = pyqtSignal(str)
+    dir_deleted = pyqtSignal(str)
+    dir_modified = pyqtSignal(str)
+    dir_moved = pyqtSignal(str, str)
+
     def __init__(self):
         super().__init__()
         self._dir = os.path.expanduser("~")
+        self._observer = Observer()
+        self._event_handler = EventHandler(self)
 
     def chdir(self, directory):
         """Change the current working directory to directory."""
@@ -31,7 +54,57 @@ class WorkingDirectoryHandler(QObject):
         if directory != self._dir:
             self._dir = directory
             os.chdir(directory)
+            self._monitor(directory)
             self.cwd_changed.emit(directory)
+
+    def _monitor(self, directory):
+        """Monitor directory with watchdog."""
+        self._observer.stop()
+        self._observer = Observer()
+        self._observer.schedule(self._event_handler, directory, recursive=False)
+        self._observer.start()
+
+
+class EventHandler(FileSystemEventHandler):
+    """Event handler to emit signals when directory content has changed.
+
+    See: https://pythonhosted.org/watchdog/api.html#event-handler-classes
+
+    Attributes:
+        _handler: WorkingDirectoryHandler to use the signals from.
+    """
+
+    def __init__(self, wd_handler):
+        super().__init__()
+        self._handler = wd_handler
+
+    def on_created(self, event):
+        logging.debug("Created %s", event.src_path)
+        if event.is_directory:
+            self._handler.dir_created.emit(event.src_path)
+        else:
+            self._handler.path_created.emit(event.src_path)
+
+    def on_deleted(self, event):
+        logging.debug("Deleted %s", event.src_path)
+        if event.is_directory:
+            self._handler.dir_deleted.emit(event.src_path)
+        else:
+            self._handler.path_deleted.emit(event.src_path)
+
+    def on_modified(self, event):
+        logging.debug("Modified %s", event.src_path)
+        if event.is_directory:
+            self._handler.dir_modified.emit(event.src_path)
+        else:
+            self._handler.path_modified.emit(event.src_path)
+
+    def on_moved(self, event):
+        logging.debug("Moved %s to %s", event.src_path, event.dest_path)
+        if event.is_directory:
+            self._handler.dir_moved.emit(event.src_path, event.dest_path)
+        else:
+            self._handler.path_moved.emit(event.src_path, event.dest_path)
 
 
 handler = WorkingDirectoryHandler()
