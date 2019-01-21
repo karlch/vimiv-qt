724ad954d05b14454cfabc6a658fc86dd4e91d7b
karlch <karlch@protonmail.com>
2019-01-21 01:59:41 +0100

    Document completion api plus quickfixes

--- vimiv/api/completion.py
+++ vimiv/api/completion.py
@@ -4,7 +4,53 @@
 # Copyright 2017-2019 Christian Karl (karlch) <karlch at protonmail dot com>
 # License: GNU GPL v3, see the "LICENSE" and "AUTHORS" files for details.
 
-"""TODO"""
+"""*Utilities to work with completion modules*.
+
+A completion module offers a model with options for command line completion and a filter
+that decides which results are filtered depending on the text in the command line. All
+completion models inherit from the :class:`BaseModel` class while the filters inherit
+from :class:`BaseFilter`.
+
+A completion module must define for which command line text it is valid. In addition, it
+can provide a custom filter as well as custom column widths for the results shown. By
+default there is only a single column which gets the complete width. Let's start with a
+simple example::
+
+    from vimiv.api import completion
+
+    class UselessModel(completion.BaseModel):
+
+        def __init__(self):
+            super().__init__(":useless")  # Gets triggered when the command line text
+                                          # starts with ":useless"
+            data = [("useless %d" % (i),) for i in range(3)]
+            self.set_data(data)
+
+The model defined offers the completions "useless 0", "useless 1" and "useless 2" if the
+command line text starts with ":useless".
+
+Note:
+    The data which was set is a sequence of tuples. The tuples are required as it is
+    possible to have multiple columns in the completion widget.
+
+To include additional information on the provided completion, further columns can be
+added::
+
+    class UselessModel(completion.BaseModel):
+
+        def __init__(self):
+            super().__init__(":useless", column_widths=(0.7, 0.3))
+            data = [("useless %d" % (i), "Integer") for i in range(3)]
+            self.set_data(data)
+
+The offered completions now provide and additional description (which is always
+"Integer") that is shown in a second column next to the actual completion. The
+description column is set up to take 30 % of the total width while the completion takes
+70 %.
+
+For an overview of implemented models, feel free to take a look at the ones defined in
+``vimiv.completion.completionmodels``.
+"""
 
 import string
 from typing import cast, Dict, NamedTuple, Sequence, Tuple
@@ -26,7 +72,7 @@ def get_module(text: str) -> "CompletionModule":
     best_match, best_match_size = cast(CompletionModule, None), -1
     for required_text, module in _modules.items():
         match_size = len(required_text)
-        if text.startswith(required_text) and len(text) > best_match_size:
+        if text.startswith(required_text) and len(required_text) > best_match_size:
             best_match, best_match_size = module, match_size
     return best_match
 
@@ -57,7 +103,7 @@ class BaseFilter(QSortFilterProxyModel):
         """Strip text of characters ignored for filtering.
 
         This default implementation strips command line prefixes and leading digits. If
-        the child class requires additional intelligence it should overwrite this
+        the child class requires additional intelligence it should override this
         method.
 
         Args:
@@ -111,7 +157,7 @@ class BaseModel(QStandardItemModel):
     """Base model used for completion models.
 
     Attributes:
-        column_widths: Tuple of floats [0..1] defining width of each column.
+        column_widths: Tuple of floats [0..1] defining the width of each column.
         text_filter: Filter class used to filter valid completions.
     """
 
@@ -162,13 +208,6 @@ class BaseModel(QStandardItemModel):
             row = [QStandardItem(elem) for elem in item]
             self.appendRow(row)
         self.sort(0)  # Sort according to the actual text
-
-
-class EmptyModel(BaseModel):
-    """Empty completion model used as fallback."""
-
-    def __init__(self):  # type: ignore
-        super().__init__("")
 
 
 class CompletionModule(NamedTuple):
