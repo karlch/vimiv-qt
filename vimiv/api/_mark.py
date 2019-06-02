36eddde4290e05c26e36743360ac9f63cc705a92
karlch <karlch@protonmail.com>
2019-06-02 14:42:35 +0200

    Implement tag support
    There are three tag commands:
    * `tag-write` to write currently marked images to a tag
    * `tag-delete` to delete a tag
    * `tag-load` to load all tagged images into the marked list

--- vimiv/api/_mark.py
+++ vimiv/api/_mark.py
@@ -6,11 +6,16 @@
 
 """Mark and tag images."""
 
-from typing import List
+
+import logging
+import os
+import shutil
+from datetime import datetime
+from typing import Any, Callable, List
 
 from PyQt5.QtCore import QObject, pyqtSignal
 
-from vimiv.utils import files, pathreceiver
+from vimiv.utils import files, pathreceiver, xdg
 from . import commands, keybindings, objreg, status
 
 
@@ -68,6 +73,71 @@ class Mark(QObject):
         for path in self._marked:
             self.marked.emit(path)
 
+    @commands.register()
+    def tag_write(self, name: str) -> None:
+        """Write marked paths to a tag.
+
+        **syntax:** ``:tag-write name``
+
+        This writes all currently marked images to this tag. This allows storing a
+        selection of marked images under a name and re-loading them in later sessions
+        using ``:tag-load name``. If the tag file exists, all marked paths that are not
+        in the tag are appended to it.
+
+        .. hint::
+            It is possible to group tags into directories by dividing the name into a
+            directory tree, e.g. ``:tag-write favourites/2017``.
+
+        positional arguments:
+            * ``name``: Name of the tag to create.
+        """
+        with Tag(name, read_only=False) as tag:
+            tag.write(self.paths)
+
+    @commands.register()
+    def tag_delete(self, name: str) -> None:
+        """Delete an existing tag.
+
+        **syntax:** ``:tag-delete name``
+
+        .. warning:: If you pass a tag group directory, the complete tree is deleted.
+
+        positional arguments:
+            * ``name``: Name of the tag to delete.
+        """
+        abspath = Tag.path(name)
+
+        def safe_delete(operation: Callable) -> None:
+            """Wrapper around delete operation logging exceptions."""
+            try:
+                operation(abspath)
+            except PermissionError:
+                raise commands.CommandError(f"permission denied")
+
+        if os.path.isfile(abspath):
+            safe_delete(os.remove)
+        elif os.path.isdir(abspath):
+            safe_delete(shutil.rmtree)
+        else:
+            raise commands.CommandError(f"tag file '{name}' does not exist")
+
+    @commands.register()
+    def tag_load(self, name: str) -> None:
+        """Load images from a tag into the current mark list.
+
+        **syntax:** ``tag-load name``
+
+        .. hint:: You can open all marked images with ``:open %m``.
+
+        positional arguments:
+            * ``name``: Name of the tag to delete.
+        """
+        with Tag(name) as tag:
+            paths = tag.read()
+        self._marked = paths
+        for path in paths:
+            self.marked.emit(path)
+
     @status.module("{mark-indicator}")
     def mark_indicator(self) -> str:
         """Indicator if the current image is marked."""
@@ -102,3 +172,89 @@ class Mark(QObject):
         except ValueError:
             self._marked.append(path)
             self.marked.emit(path)
+
+
+class Tag:
+    """Contextmanager to work with tag files.
+
+    Usage:
+        >>> # For reading
+        >>> with Tag("name", read_only=True) as tag:
+        >>>    paths = tag.read()
+
+        >>> # For writing
+        >>> with Tag("name", read_only=False) as tag:
+        >>>    tag.write(["path1", "path2", ...])
+
+    Class attributes:
+        COMMENTCHAR: Character prepended to comment lines.
+
+    Attributes:
+        name: Name of the tag file.
+        mode: File mode used for open.
+
+        _file: The associated file handler.
+    """
+
+    COMMENTCHAR = "#"
+
+    def __init__(self, name: str, read_only: bool = True):
+        self.name = name
+        abspath = Tag.path(name)
+        exists = os.path.isfile(abspath)
+        self.mode = "r" if read_only else ("r+" if exists else "a+")
+        logging.debug("Opened tag object: %s", self)
+        os.makedirs(os.path.dirname(abspath), exist_ok=True)
+        self._file = open(abspath, self.mode)
+
+        if read_only:
+            logging.debug("%s: Reading tag file", self)
+        elif not exists:
+            logging.debug("%s: Creating new tag file", self)
+            self._write_header()
+        else:
+            logging.debug("%s: Appending to existing tag file", self)
+
+    def __enter__(self) -> "Tag":
+        return self
+
+    def __exit__(self, *_: Any) -> None:
+        self._file.close()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}({self.name}, mode={self.mode})"
+
+    def write(self, paths: List[str]) -> None:
+        """Write paths to the tag file."""
+        existing = {path.strip() for path in self.read()}
+        new_paths = set(paths) - existing
+        self._file.write("\n".join(sorted(new_paths)) + "\n")
+
+    def read(self) -> List[str]:
+        """Read paths from the tag file."""
+        paths = [
+            path.strip() for path in self._file if not path.startswith(Tag.COMMENTCHAR)
+        ]
+        logging.debug("%s: read %d paths from tag", self, len(paths))
+        return paths
+
+    @staticmethod
+    def dirname() -> str:
+        """Return path to the tag directory."""
+        return xdg.join_vimiv_data("tags")
+
+    @staticmethod
+    def path(name: str) -> str:
+        """Return absolute path to a tag file called name."""
+        return os.path.join(Tag.dirname(), name)
+
+    def _write_header(self) -> None:
+        """Write header to a new tag file."""
+        self._write_comment("vimiv tag file")
+        now = datetime.now()
+        formatted_time = now.strftime("%Y-%m-%d %H:%M")
+        self._write_comment(f"created: {formatted_time}")
+
+    def _write_comment(self, comment: str) -> None:
+        """Write a comment line to the tag file."""
+        self._file.write(f"{Tag.COMMENTCHAR} {comment}\n")
