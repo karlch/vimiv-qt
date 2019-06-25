71de9b3561c97c22563971cbd88d01f048593872
karlch <karlch@protonmail.com>
2019-06-25 09:03:28 +0200

    Add QSlider with value widget
    This version of QSlider additionally paints the current value at the
    center.

--- vimiv/gui/widgets.py
+++ vimiv/gui/widgets.py
@@ -7,6 +7,7 @@
 """Simple base QtWidgets to inherit from."""
 
 from PyQt5.QtCore import QItemSelectionModel, QMargins, Qt
+from PyQt5.QtGui import QPainter, QFontMetrics
 from PyQt5.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
@@ -14,9 +15,11 @@ from PyQt5.QtWidgets import (
     QTreeView,
     QAbstractItemView,
     QLabel,
+    QSlider,
 )
 
 from vimiv.config import styles
+from vimiv.utils import cached_method
 
 
 class SimpleGrid(QGridLayout):
@@ -85,6 +88,73 @@ class FlatTreeView(QTreeView):
         """Return the currently selected row."""
         selected_indexes = self.selectionModel().selectedIndexes()  # 3 columns
         return selected_indexes[0].row()
+
+
+class SliderWithValue(QSlider):
+    """QSlider displaying the current value at the center.
+
+    Attributes:
+        _padding: Vertical padding in pixels between text and slider bar.
+        _handle_margin: Vertical size the handle goes over the slider bar in pixels.
+        _handle_width: Width of the handle in pixels.
+    """
+
+    STYLESHEET = ""  # Dummy that gets extended by custom dynamic styling
+
+    PADDING = 2
+    HANDLE_MARGIN = 5
+
+    def __init__(self, left_color, handle_color, right_color, *args, parent=None):
+        super().__init__(*args, parent=parent)
+        self._left_color = left_color
+        self._handle_color = handle_color
+        self._right_color = right_color
+
+    def paintEvent(self, event):
+        """Override paint event to additionally paint the current value."""
+        super().paintEvent(event)
+        self._init_style()
+
+        text = str(self.value())
+        rect = self.geometry()
+        painter = QPainter(self)
+        font_metrics = QFontMetrics(self.font())
+
+        x = (rect.width() - font_metrics.width(text)) / 2
+        y = rect.height() - (rect.height() - font_metrics.capHeight()) / 2
+        painter.drawText(x, y, text)
+
+    @cached_method
+    def _init_style(self):
+        """Initialize the stylesheet.
+
+        This is called on the first paintEvent to ensure all sizes and fonts are
+        correct.
+        """
+        font_height = QFontMetrics(self.font()).capHeight()
+        groove_height = font_height + 2 * self.PADDING
+        total_height = groove_height + 2 * self.HANDLE_MARGIN
+        sheet = f"""
+        QSlider {{
+            height: {total_height:d}px;
+        }}
+
+        QSlider::sub-page {{
+            background-color: {self._left_color};
+        }}
+
+        QSlider::handle {{
+            width: 24px;
+            background-color: {self._handle_color};
+            margin: -{SliderWithValue.HANDLE_MARGIN}px 0px;
+        }}
+
+        QSlider::groove {{
+            background-color: {self._right_color};
+            height: {groove_height}px;
+        }}
+        """
+        styles.apply(self, append=sheet)
 
 
 class ImageLabel(QLabel):
