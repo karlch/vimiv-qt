39645ca6578d4518f4e72f8dd7fe64d18c20d228
karlch <karlch@protonmail.com>
2019-11-17 17:58:05 +0100

    Fix lint and mypy for aboutToQuit signal

--- vimiv/app.py
+++ vimiv/app.py
@@ -39,7 +39,7 @@ class Application(QApplication):
     @staticmethod
     def preexit(returncode: int) -> None:
         """Prepare exit by finalizing any running threads."""
-        Application.instance.aboutToQuit.emit()
+        QApplication.instance().aboutToQuit.emit()  # type: ignore
         # Do not start any new threads
         utils.Pool.clear()
         # Wait for any running threads to exit safely
