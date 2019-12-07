karlch/vimiv-qt

commit fb5cebe767a4cf50723dc7db8de6aabe63999423

Unify use of tuple in argparser as empty iterable

vimiv/parser.py
@@ -56,7 +56,7 @@ def get_argparser() -> argparse.ArgumentParser:
         "-s",
         "--set",
         nargs=2,
-        default=[],
+        default=(),
         action="append",
         dest="cmd_settings",
         metavar=("OPTION", "VALUE"),

commit 2083080ee78f1e5e39f763dbfb3226aa40c57110

Fix -s commandline option
Broken since fb5cebe767a4cf50723dc7db8de6aabe63999423 as tuple does not
support append.

vimiv/parser.py
@@ -56,7 +56,7 @@ def get_argparser() -> argparse.ArgumentParser:
         "-s",
         "--set",
         nargs=2,
-        default=(),
+        default=[],  # List is required for action="append"
         action="append",
         dest="cmd_settings",
         metavar=("OPTION", "VALUE"),

tests/unit/test_parser.py
@@ -86,3 +86,12 @@ def test_log_level():
 def test_fail_log_level():
     with pytest.raises(argparse.ArgumentTypeError, match="Invalid log level"):
         parser.loglevel("other")
+
+
+def test_parse_settings(argparser):
+    settings = [["statusbar.show", "false"], ["style", "default"]]
+    arglist = []
+    for name, value in settings:
+        arglist.extend(("-s", name, value))
+    args = argparser.parse_args(arglist)
+    assert args.cmd_settings == settings