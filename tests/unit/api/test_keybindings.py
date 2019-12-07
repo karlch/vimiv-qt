9347c85d6a505d5b2218597cb60e9192cee6c18a
karlch <karlch@protonmail.com>
2019-12-07 13:35:46 +0100

    Add tests for partial matching with special keys

--- tests/unit/api/test_keybindings.py
+++ tests/unit/api/test_keybindings.py
@@ -6,8 +6,6 @@
 
 """Tests for vimiv.config.keybindings."""
 
-# from vimiv.api import keybindings
-# from vimiv.modes import Modes
 from vimiv import api
 
 
@@ -39,3 +37,33 @@ def test_bind_unbind_keybinding():
     assert bindings["t2"].value == "test"
     api.keybindings.unbind("t2", api.modes.IMAGE)
     assert "t2" not in bindings
+
+
+def test_partial_matches():
+    bindings_dict = {"t1": "test", "t2": "test"}
+    bindings = api.keybindings._BindingsTrie()
+    bindings.update(**bindings_dict)
+    match = bindings.match("t")
+    assert match.is_partial_match
+    assert list(match.partial) == list(bindings_dict.items())
+
+
+def test_partial_matches_with_special_keys():
+    bindings_dict = {"<ctrl>a": "test", "<ctrl>b": "test"}
+    bindings = api.keybindings._BindingsTrie()
+    bindings.update(**bindings_dict)
+    assert bindings.match("<").is_no_match
+    match = bindings.match(("<ctrl>",))
+    assert match.is_partial_match
+    assert list(match.partial) == list(bindings_dict.items())
+
+
+def test_partial_matches_with_multiple_special_keys():
+    bindings_dict = {"<ctrl><alt>a": "test", "<ctrl><alt>b": "test"}
+    bindings = api.keybindings._BindingsTrie()
+    bindings.update(**bindings_dict)
+    assert bindings.match("<").is_no_match
+    assert bindings.match(("<ctrl>", "<")).is_no_match
+    match = bindings.match(("<ctrl>", "<alt>"))
+    assert match.is_partial_match
+    assert list(match.partial) == list(bindings_dict.items())
