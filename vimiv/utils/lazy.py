d697867382343869dee83e70505786ee0dd65540
karlch <karlch@protonmail.com>
2020-01-18 17:40:15 +0100

    Add a module for lazy imports
    This is especially useful for optional imports which, as they are
    optional, probably are only required during runtime, not immediately
    upon startup.

---
+++ vimiv/utils/lazy.py
@@ -0,0 +1,66 @@
+# vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+
+# This file is part of vimiv.
+# Copyright 2017-2020 Christian Karl (karlch) <karlch at protonmail dot com>
+# License: GNU GPL v3, see the "LICENSE" and "AUTHORS" files for details.
+
+"""Provides the import_module function for lazy importing."""
+
+import sys
+import importlib.util
+from functools import lru_cache
+
+
+class Module:
+    """Wrapper class for a lazy-loaded module.
+
+    Attributes:
+        fullname: Full name of the wrapped module.
+
+        _module: The imported module once loaded.
+    """
+
+    def __init__(self, fullname):
+        self.fullname = fullname
+        self._module = None
+
+    @classmethod
+    @lru_cache(None)
+    def factory(cls, fullname, *, optional=False):
+        """Create a new class instance for the module defined by fullname.
+
+        If optional is True, None is returned if the module cannot be found instead of
+        raising ModuleNotFoundError. This method is cached so we only create one
+        instance per name of a module.
+        """
+        if importlib.util.find_spec(fullname) is None:
+            if optional:
+                return None
+            raise ModuleNotFoundError(f"No module named '{fullname}'")
+        return cls(fullname)
+
+    @property
+    def module(self):
+        """The imported wrapped module."""
+        if self._module is None:
+            self._module = importlib.import_module(self.fullname)
+        return self._module
+
+    def __getattr__(self, name):
+        """Retrieve attributes from the wrapped module."""
+        return getattr(self.module, name)
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}({self.fullname})"
+
+
+def import_module(fullname, *, optional=False):
+    """Lazy import a module by its full name.
+
+    If the module was already imported, returns it from sys.modules. Otherwise returns a
+    lazy Module instance. See Module.factory for the creation details.
+    """
+    try:
+        return sys.modules[fullname]
+    except KeyError:
+        return Module.factory(fullname, optional=optional)
