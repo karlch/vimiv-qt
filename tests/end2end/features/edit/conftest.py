dbd6227b22c34448a0208bdef94613fd23b2ab98
karlch <karlch@protonmail.com>
2020-01-19 19:56:43 +0100

    Add end2end tests for straighten widget

---
+++ tests/end2end/features/edit/conftest.py
@@ -0,0 +1,26 @@
+# vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+
+# This file is part of vimiv.
+# Copyright 2017-2020 Christian Karl (karlch) <karlch at protonmail dot com>
+# License: GNU GPL v3, see the "LICENSE" and "AUTHORS" files for details.
+
+import pytest_bdd as bdd
+
+from vimiv.parser import geometry
+
+
+@bdd.then(bdd.parsers.parse("the image size should be {size}"))
+def ensure_size(size, image):
+    expected = geometry(size)
+    image_rect = image.sceneRect()
+    assert expected.width() == image_rect.width()
+    assert expected.height() == image_rect.height()
+
+
+@bdd.then(bdd.parsers.parse("the image size should not be {size}"))
+def ensure_size_not(size, image):
+    expected = geometry(size)
+    image_rect = image.sceneRect()
+    width_neq = expected.width() != image_rect.width()
+    height_neq = expected.height() != image_rect.height()
+    assert width_neq or height_neq
