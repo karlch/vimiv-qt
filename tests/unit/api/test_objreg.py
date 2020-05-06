22ef066aa5c6d2c567ae76dc087009853bc4da09
karlch <karlch@protonmail.com>
2020-05-06 21:53:53 +0200

    Extend objreg unit test to test call_with_instance

--- tests/unit/api/test_objreg.py
+++ tests/unit/api/test_objreg.py
@@ -6,19 +6,42 @@
 
 """Tests for vimiv.api.objreg."""
 
+import pytest
+
 from vimiv.api import objreg
 
 
-class DummyObject:
-    """DummyObject to store in the registry."""
+class Multiplier:
+    """Test class to be registered in the objreg with a simple test method."""
 
     instance = None
 
     @objreg.register
-    def __init__(self):
-        pass
+    def __init__(self, factor):
+        self._factor = factor
+
+    def multiply_by(self, number):
+        return number * self._factor
 
 
-def test_register_object_via_decorator():
-    dummy = DummyObject()
-    assert DummyObject.instance == dummy
+@pytest.fixture()
+def multiply_by_two():
+    """Fixture to create a multiplier instance that multiplies by two."""
+    multiplier = Multiplier(2)
+    yield multiplier
+    Multiplier.instance = None
+
+
+def test_call_with_instance(multiply_by_two):
+    number = 42
+    result = objreg._call_with_instance(Multiplier.multiply_by, number)
+    assert result == 2 * number
+
+
+def test_call_without_instance():
+    def func(argument):
+        return argument * 2
+
+    argument = 42
+
+    assert func(argument) == objreg._call_with_instance(func, argument)
