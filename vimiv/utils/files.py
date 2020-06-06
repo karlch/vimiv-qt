ff36984b5136a6133f799ad6694a3c4f98d82b82
karlch <karlch@protonmail.com>
2020-06-06 17:34:19 +0200

    Fix type annotation of overridden imghdr jpg tests

--- vimiv/utils/files.py
+++ vimiv/utils/files.py
@@ -172,7 +172,7 @@ add_image_format("svg", test_svg)
 
 # We now directly override the jpg part of imghdr as it has some known limitations
 # See e.g. https://bugs.python.org/issue16512
-def test_jpg(h: bytes, _f: BinaryIO) -> Optional[str]:
+def test_jpg(h: bytes, _f: Optional[BinaryIO]) -> Optional[str]:
     """Custom jpg test function.
 
     The one from the imghdr module fails with some jpgs that include ICC_PROFILE data.
@@ -184,7 +184,7 @@ def test_jpg(h: bytes, _f: BinaryIO) -> Optional[str]:
     return None
 
 
-def test_jpg_fallback(h: bytes, _f: BinaryIO) -> Optional[str]:
+def test_jpg_fallback(h: bytes, _f: Optional[BinaryIO]) -> Optional[str]:
     """Fallback test for jpg files with no headers, only the two starting bits."""
     return "jpg" if h[:2] == b"\xff\xd8" else None
 
