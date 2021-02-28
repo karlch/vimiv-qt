a2ea5c8b4c573510ba5e29b2282485ae2b367d60
karlch <karlch@protonmail.com>
2021-02-28 14:00:06 +0100

    Fix typo in avis file header

--- vimiv/plugins/imageformats.py
+++ vimiv/plugins/imageformats.py
@@ -38,7 +38,7 @@ def test_cr2(header: bytes, _f: Optional[BinaryIO]) -> bool:
 
 
 def test_avif(header: bytes, _f: Optional[BinaryIO]) -> bool:
-    return header[4:12] in (b"ftypavif", b"ftypeavis")
+    return header[4:12] in (b"ftypavif", b"ftypavis")
 
 
 FORMATS = {
