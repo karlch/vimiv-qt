261608e05ec2401058119e98e5e80462ea3ae83a
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-04-09 21:03:03 +0200

    NEW: end2end test copy-image

--- tests/end2end/features/misc/conftest.py
+++ tests/end2end/features/misc/conftest.py
@@ -4,7 +4,7 @@
 # Copyright 2017-2021 Christian Karl (karlch) <karlch at protonmail dot com>
 # License: GNU GPL v3, see the "LICENSE" and "AUTHORS" files for details.
 
-from PyQt5.QtGui import QGuiApplication, QClipboard
+from PyQt5.QtGui import QGuiApplication, QClipboard, QImage
 
 import pytest
 import pytest_bdd as bdd
@@ -23,3 +23,33 @@ def check_clipboard(clipboard, text):
 @bdd.then(bdd.parsers.parse("The primary selection should contain {text}"))
 def check_primary(clipboard, text):
     assert text in clipboard.text(mode=QClipboard.Selection)
+
+
+@bdd.then(bdd.parsers.parse("The clipboard should contain image {image}"))
+def check_clipboard_image(clipboard, image):
+    assert clipboard.pixmap(mode=QClipboard.Clipboard).toImage() == QImage(image)
+
+
+@bdd.then(bdd.parsers.parse("The primary selection should contain image {image}"))
+def check_primary_image(clipboard, image):
+    assert clipboard.pixmap(mode=QClipboard.Selection).toImage() == QImage(image)
+
+
+@bdd.then(bdd.parsers.parse("The clipboard should contain an image with width {width}"))
+def check_clipboard_image_width(clipboard, width):
+    assert clipboard.pixmap(mode=QClipboard.Clipboard).size().width() == int(width)
+
+
+@bdd.then(
+    bdd.parsers.parse("The clipboard should contain an image with height {height}")
+)
+def check_clipboard_image_height(clipboard, height):
+    assert clipboard.pixmap(mode=QClipboard.Clipboard).size().height() == int(height)
+
+
+@bdd.then(bdd.parsers.parse("The clipboard should contain an image with size {size}"))
+def check_clipboard_image_size(clipboard, size):
+    assert max(
+        clipboard.pixmap(mode=QClipboard.Clipboard).size().height(),
+        clipboard.pixmap(mode=QClipboard.Clipboard).size().width(),
+    ) == int(size)
