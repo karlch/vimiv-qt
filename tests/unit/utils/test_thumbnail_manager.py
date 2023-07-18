8d9118e358c0a9d757817e4d77ade834fa16c1d9
buzzingwires <buzzingwires@outlook.com>
2023-07-18 02:55:15 -0400

    Don't manually specify indices when testing async thumbnail loading.

--- tests/unit/utils/test_thumbnail_manager.py
+++ tests/unit/utils/test_thumbnail_manager.py
@@ -25,11 +25,15 @@ def manager(qtbot, tmp_path, mocker):
     yield thumbnail_manager.ThumbnailManager(None)
 
 
+def _create_thumbnails(manager, paths):
+    manager.create_thumbnails_async(range(0, len(paths)), paths)
+
+
 def test_thumbnail_save_disabled(monkeypatch, qtbot, tmp_path, manager):
     monkeypatch.setattr(settings.thumbnail.save, "value", False)
     no_thumbnail_path = str(tmp_path / "no_thumbnail.jpg")
     QPixmap(300, 300).save(no_thumbnail_path, "jpg")
-    manager.create_thumbnails_async([0], [no_thumbnail_path])
+    _create_thumbnails(manager, [no_thumbnail_path])
     check_thumbails_created(qtbot, manager, 0)
 
 
@@ -37,13 +41,13 @@ def test_thumbnail_save_disabled_no_delete_old(monkeypatch, qtbot, tmp_path, man
     monkeypatch.setattr(settings.thumbnail.save, "value", True)
     has_thumbnail_path = str(tmp_path / "has_thumbnail.jpg")
     QPixmap(300, 300).save(has_thumbnail_path, "jpg")
-    manager.create_thumbnails_async([0], [has_thumbnail_path])
+    _create_thumbnails(manager, [has_thumbnail_path])
     check_thumbails_created(qtbot, manager, 1)
 
     monkeypatch.setattr(settings.thumbnail.save, "value", False)
     no_thumbnail_path = str(tmp_path / "no_thumbnail.jpg")
     QPixmap(300, 300).save(no_thumbnail_path, "jpg")
-    manager.create_thumbnails_async([0, 1], [has_thumbnail_path, no_thumbnail_path])
+    _create_thumbnails(manager, [has_thumbnail_path, no_thumbnail_path])
     check_thumbails_created(qtbot, manager, 1)
 
 
@@ -53,18 +57,18 @@ def test_create_n_thumbnails(qtbot, tmp_path, manager, n_paths):
     filenames = [str(tmp_path / f"image_{i}.jpg") for i in range(n_paths)]
     for filename in filenames:
         QPixmap(300, 300).save(filename, "jpg")
-    manager.create_thumbnails_async(range(0, len(filenames)), filenames)
+    _create_thumbnails(manager, filenames)
     check_thumbails_created(qtbot, manager, n_paths)
 
 
 def test_create_thumbnails_for_non_existing_path(qtbot, manager):
-    manager.create_thumbnails_async([0], ["this/is/not/a/path"])
+    _create_thumbnails(manager, ["this/is/not/a/path"])
     check_thumbails_created(qtbot, manager, 0)
 
 
 def test_create_thumbnails_for_non_image_path(qtbot, tmp_path, manager):
     filename = str(tmp_path / "image.jpg")
-    manager.create_thumbnails_async([0], [filename])
+    _create_thumbnails(manager, [filename])
     check_thumbails_created(qtbot, manager, 0)
 
 
@@ -73,7 +77,7 @@ def test_do_not_create_thumbnail_for_thumbnail(qtbot, manager):
         manager.directory, hashlib.md5(b"thumbnail").hexdigest() + ".png"
     )
     QPixmap(256, 256).save(filename)
-    manager.create_thumbnails_async([0], [filename])
+    _create_thumbnails(manager, [filename])
     check_thumbails_created(qtbot, manager, 1)
 
 
